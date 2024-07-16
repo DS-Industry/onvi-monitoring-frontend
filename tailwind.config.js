8498098fad0dd1c329eaf673e32006f39cf8526e
Abhishek <abhishek@moy-ka.com>
2024-07-16 13:21:50 +0530

    added default theme colors in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary01: "#BFFA00",
+        primary02: "#0B68E1",
+        primary02_Hover: "#094DA7",
+        text02: "#747677",
+        text01: "#000000",
+        text03: "#D8D9DD",
+        text04: "#FFFFFF",
+        textError: "#FF3B30",
+        successFill: "#00A355",
+        errorFill:"#EB5757",
+        background01: "#1F1F1F",
+        background02: "#FFFFFF",
+        background05: "#F8F8FA",
+        background06: "#E4F0FF",
+        opacity01: "#D8D9DD",
+      },
+      boxShadow: {
+        card: "0 2px 13px -2px rgba(27, 33, 44, 0.12)",
+      },
+    },
   },
   plugins: [],
 };
