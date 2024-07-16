d99b61817110471ab4d77d3e34e3d8cd466247fc
Abhishek <abhishek@moy-ka.com>
2024-07-16 13:42:22 +0530

    added React Router DOM and configure some demo routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Root from "./Root";
+import ErrorPage from "./pages/Error";
 
-import './App.css'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "", element: <Home /> },
+      {
+        path: "Администрирование",
+        element: <About />,
+      },
+      { path: "Администрирование/sub1", element: <Home /> },
+      { path: "Администрирование/sub2", element: <Services /> },
+      { path: "Администрирование/sub3", element: <Contact /> },
+      { path: "Администрирование/sub4", element: <Home /> },
+      { path: "Администрирование/sub5", element: <Services /> },
+      { path: "Станция", element: <Services /> },
+      { path: "Hr", element: <Contact /> },
+      { path: "Финансы", element: <Home /> },
+      { path: "Анализ", element: <About /> },
+      { path: "Лояльность", element: <Services /> },
+      { path: "Оборудование", element: <Contact /> },
+      { path: "Склад", element: <Contact /> },
+    ],
+  },
+]);
 
-function App() {
+const App: React.FC = () => {
+  return <RouterProvider router={router} />;
+};
 
-
-  return (
-    <>
-      <div className='text-5xl text-red-500'>hello</div>
-    </>
-  )
-}
-
-export default App
+export default App;
