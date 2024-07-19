23f4cd92254f4e2339ad49e42b915ae2c700e3bd
Abhishek <abhishek@moy-ka.com>
2024-07-19 18:21:18 +0530

    Filter hide and show btn added.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import Filter from "../components/ui/Filter";
+import ArrowDown from "../assets/icons/keyboard_arrow_down.svg?react";
+import ArrowUp from "../assets/icons/keyboard_arrow_up.svg?react";
 
 const About = () => {
-  return <div>
-    <Filter/>
-  </div>;
+  const [filterOpen, setFilterOpen] = useState(false);
+
+  return (
+    <>
+      <button
+        onClick={() => setFilterOpen(!filterOpen)}
+        className="flex font-semibold text-primary02"
+      >
+        Свернуть фильтр {filterOpen ? <ArrowDown /> : <ArrowUp />}
+      </button>
+      {filterOpen && <Filter />}
+      <div className="mt-8">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam,
+        voluptas mollitia nesciunt ea nihil corporis dolorem labore, ratione
+        odio inventore a. Numquam illum distinctio facere suscipit et, ratione
+        veniam labore!
+      </div>
+    </>
+  );
 };
 
 export default About;
