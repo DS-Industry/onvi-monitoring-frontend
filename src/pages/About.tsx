23b8d8f9aca78e8d720a38767a24e5882544669d
Abhishek <abhishek@moy-ka.com>
2024-07-22 19:05:22 +0530

    created the overflow table.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Filter from "../components/ui/Filter";
 import ArrowDown from "../assets/icons/keyboard_arrow_down.svg?react";
 import ArrowUp from "../assets/icons/keyboard_arrow_up.svg?react";
+import OverflowTable from "../components/ui/OverflowTable";
 
 const About = () => {
   const [filterOpen, setFilterOpen] = useState(false);
@@ -12,14 +13,11 @@ const About = () => {
         onClick={() => setFilterOpen(!filterOpen)}
         className="flex font-semibold text-primary02"
       >
-        Свернуть фильтр {filterOpen ? <ArrowDown /> : <ArrowUp />}
+        Свернуть фильтр {filterOpen ? <ArrowUp /> : <ArrowDown />}
       </button>
       {filterOpen && <Filter />}
-      <div className="mt-8">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam,
-        voluptas mollitia nesciunt ea nihil corporis dolorem labore, ratione
-        odio inventore a. Numquam illum distinctio facere suscipit et, ratione
-        veniam labore!
+      <div className="container mt-8">
+        <OverflowTable />
       </div>
     </>
   );
