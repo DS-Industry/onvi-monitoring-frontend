eea6ae3d60156a8cdd121b09330845e72fd5d52e
Abhishek <abhishek@moy-ka.com>
2024-07-23 16:11:53 +0530

    Created UI if there no data or table to show.

---
+++ src/components/ui/NoDataUI.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+type Props = {
+  title: string;
+  description: string;
+  children: React.ReactNode;
+};
+
+const NoDataUI: React.FC<Props> = ({ title, description, children }: Props) => {
+  return (
+    <div className=" flex justify-center items-center h-full">
+      <div className="text-center">
+        {children}
+        <h3 className="font-semibold text-2xl">{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default NoDataUI;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,8 +6,11 @@ import Edit from "../assets/icons/edit.svg?react";
 import OverflowTable from "../components/ui/OverflowTable";
 import Modal from "../components/ui/Modal";
 import TableSettings from "../components/ui/TableSettings";
+import NoDataUI from "../components/ui/NoDataUI";
+import SalyIamge from "../assets/Saly-45.svg?react";
 
 const About = () => {
+  const [isData, setIsData] = useState(false);
   const [filterOpen, setFilterOpen] = useState(false);
   const contentRef = useRef<HTMLDivElement>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -30,30 +33,46 @@ const About = () => {
   return (
     <>
       <button
+        disabled={!isData}
         onClick={() => setFilterOpen(!filterOpen)}
-        className="flex font-semibold text-primary02"
+        className={`flex font-semibold text-primary02 ${
+          isData ? "opacity-100" : "opacity-50"
+        }`}
       >
         Свернуть фильтр {filterOpen ? <ArrowUp /> : <ArrowDown />}
       </button>
+
       <div
         ref={contentRef}
         className={`overflow-hidden transition-all duration-500 ease-in-out max-h-0`}
       >
         {filterOpen && <Filter />}
       </div>
-      <div className="container mt-8">
-        <OverflowTable />
-      </div>
-      <button
-        onClick={openModal}
-        className="text-primary02 text-sm font-semibold flex items-center gap-2 mt-1.5 py-2"
-      >
-        Настройки таблицы <Edit />
-      </button>
+   
+      {isData ? (
+        <>
+          <div className="container mt-8">
+            <OverflowTable />
+          </div>
+          <button
+            onClick={openModal}
+            className="text-primary02 text-sm font-semibold flex items-center gap-2 mt-1.5 py-2"
+          >
+            Настройки таблицы <Edit />
+          </button>
 
-      <Modal isOpen={isModalOpen} onClose={closeModal}>
-        <TableSettings />
-      </Modal>
+          <Modal isOpen={isModalOpen} onClose={closeModal}>
+            <TableSettings />
+          </Modal>
+        </>
+      ) : (
+        <NoDataUI
+          title="В этом разделе представлена статистика уборки постов"
+          description="Данные вводятся автоматически "
+        >
+          <SalyIamge  className="mx-auto"/>
+        </NoDataUI>
+      )}
     </>
   );
 };
