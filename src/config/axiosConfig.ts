4d497135d687ae3a00cd8d45752cdaf61d705dfe
devang-garg <devang@moy-ka.com>
2025-01-31 16:15:57 +0530

    Added the russian translations for the error files.

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import useAuthStore from "@/config/store/authSlice";
+import { useTranslation } from "react-i18next";
 
 let showSnackbar: (message: string, type: "success" | "error" | "info" | "warning") => void;
 
@@ -30,25 +31,26 @@ api.interceptors.request.use(
 
 api.interceptors.response.use(
   (response) => {
-    if (showSnackbar) {
-      const successStatuses = [200, 201];
-      if (successStatuses.includes(response.status) && response.config.method !== "get") {
-        showSnackbar("Your request has been processed!", "success");
-      }
-    } else {
-      console.error("Snackbar function not initialized.");
-    }
+    // if (showSnackbar) {
+    //   const successStatuses = [200, 201];
+    //   if (successStatuses.includes(response.status) && response.config.method !== "get") {
+    //     showSnackbar("Your request has been processed!", "success");
+    //   }
+    // } else {
+    //   console.error("Snackbar function not initialized.");
+    // }
     return response;
   },
   (error) => {
     if (showSnackbar) {
+      const { t } = useTranslation();
       const { response } = error;
 
       // Extract error message from API response if it exists
       const apiMessage = response?.data?.message;
       if (apiMessage) {
         // Show the error message from the API response
-        showSnackbar(apiMessage, "error");
+        showSnackbar(t(`errors.${String(response?.data?.code)}`), "error");
       } else {
         showSnackbar("An error occurred. Please try again.", "error");
       }
