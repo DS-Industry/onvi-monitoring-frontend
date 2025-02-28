86e01983b8a50d74ed8361ac0e22da3a84a4b402
devang-garg <devang@moy-ka.com>
2025-02-28 20:26:16 +0530

    Added bar charts for programs.

--- src/pages/Pos/ProgramDevices.tsx
+++ src/pages/Pos/ProgramDevices.tsx
@@ -8,8 +8,21 @@ import { useLocation } from "react-router-dom";
 import FilterMonitoring from "@ui/Filter/FilterMonitoring.tsx";
 import SalyIamge from "@/assets/Saly-45.svg?react";
 import TableSkeleton from "@/components/ui/Table/TableSkeleton";
-import { usePosType, useStartDate, useEndDate, useSetPosType, useSetStartDate, useSetEndDate } from '@/hooks/useAuthStore'; 
+import { usePosType, useStartDate, useEndDate, useSetPosType, useSetStartDate, useSetEndDate } from '@/hooks/useAuthStore';
 import { getPoses } from "@/services/api/equipment";
+import { Bar } from "react-chartjs-2";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+} from "chart.js";
+
+// Register Chart.js Components
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 interface FilterDepositPos {
     dateStart: Date;
@@ -95,6 +108,14 @@ const ProgramDevices: React.FC = () => {
         (item) => ({ name: item.name, value: item.id.toString() })
     );
 
+    const getRandomColor = (index: number) => {
+        const colors = [
+            "#FF5733", "#33FF57", "#3357FF", "#F39C12", "#8E44AD",
+            "#1ABC9C", "#D35400", "#7F8C8D", "#27AE60", "#C0392B"
+        ];
+        return colors[index % colors.length]; // Cycle through colors if needed
+    };
+
     return (
         <>
             <FilterMonitoring
@@ -108,17 +129,105 @@ const ProgramDevices: React.FC = () => {
                 isTableLoading || filterLoading ? (<TableSkeleton columnCount={columnsProgramsPos.length} />)
                     :
                     devicePrograms.length > 0 ? (
-                        <div className="mt-8">
-                            {devicePrograms.map((deviceProgram) =>
-                                <OverflowTable
-                                    title={deviceProgram.name}
-                                    urlTitleId={deviceProgram.id}
-                                    nameUrlTitle={"/station/programs/devices"}
-                                    tableData={deviceProgram.programsInfo}
-                                    columns={columnsProgramsPos}
-                                />
-                            )
-                            }
+                        <div className="mt-8 space-y-6">
+                            <div className="flex space-x-2">
+                                <div className="w-[561px] h-80 shadow-card rounded-2xl p-4">
+                                    <Bar
+                                        data={{
+                                            labels: devicePrograms.flatMap(program =>
+                                                program.programsInfo.flatMap(info => [info.programName, ""]) // Adding empty labels as gaps
+                                            ),
+                                            datasets: [
+                                                {
+                                                    label: "Кол-во авто",
+                                                    data: devicePrograms.flatMap(program =>
+                                                        program.programsInfo.flatMap(info => [info.counter, NaN]) // NaN creates the gap
+                                                    ),
+                                                    backgroundColor: devicePrograms.flatMap(program =>
+                                                        program.programsInfo.flatMap((_info, index) => [getRandomColor(index), "rgba(0,0,0,0)"]) // Unique color + transparent gap
+                                                    ),
+                                                },
+                                            ],
+                                        }}
+                                        options={{
+                                            indexAxis: "y" as const,
+                                            responsive: true,
+                                            plugins: {
+                                                legend: { display: false },
+                                                title: {
+                                                    display: true,
+                                                    text: "Программы",
+                                                    font: { size: 16 },
+                                                }
+                                            },
+                                            scales: {
+                                                x: { beginAtZero: true },
+                                                y: { ticks: { font: { size: 12 } } },
+                                            },
+                                        }}
+                                    />
+                                    <div className="flex items-center justify-center ml-20 text-text01">Кол-во авто</div>
+                                </div>
+                                <div className="w-[561px] h-80 shadow-card rounded-2xl p-4">
+                                    <Bar
+                                        data={{
+                                            labels: devicePrograms.flatMap(program =>
+                                                program.programsInfo.map(info => info.programName) // No empty labels
+                                            ),
+                                            datasets: [
+                                                {
+                                                    label: "Кол-во авто",
+                                                    data: devicePrograms.flatMap(program =>
+                                                        program.programsInfo.map(info => info.totalTime) // No NaN values
+                                                    ),
+                                                    backgroundColor: devicePrograms.flatMap(program =>
+                                                        program.programsInfo.map((_info, index) => getRandomColor(index))
+                                                    ),
+                                                },
+                                            ],
+                                        }}
+                                        options={{
+                                            indexAxis: "x" as const,
+                                            responsive: true,
+                                            plugins: {
+                                                legend: { display: false },
+                                                title: {
+                                                    display: true,
+                                                    text: "Выручка по программам",
+                                                    font: { size: 16 },
+                                                }
+                                            },
+                                            scales: {
+                                                y: { beginAtZero: true },
+                                                x: {
+                                                    ticks: {
+                                                        autoSkip: false,
+                                                        font: { size: 12 },
+                                                        maxRotation: 90,
+                                                        minRotation: 90,
+                                                        color: function (context) {
+                                                            return getRandomColor(context.index);
+                                                        }
+                                                    }
+                                                }
+                                            },
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                            <div className="shadow-card rounded-2xl p-4 space-y-6">
+                                <div className="text-text01 font-semibold text-2xl">Отчет по выручке</div>
+                                {devicePrograms.map((deviceProgram) =>
+                                    <OverflowTable
+                                        title={deviceProgram.name}
+                                        urlTitleId={deviceProgram.id}
+                                        nameUrlTitle={"/station/programs/devices"}
+                                        tableData={deviceProgram.programsInfo}
+                                        columns={columnsProgramsPos}
+                                    />
+                                )
+                                }
+                            </div>
                         </div>
                     ) : (
                         <>
