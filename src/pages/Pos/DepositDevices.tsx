8ae05a7000f9f0bae91b66d6490f146a00b3bc79
devang-garg <devang@moy-ka.com>
2025-03-11 18:27:34 +0530

    Corrected the pos response issue.

--- src/pages/Pos/DepositDevices.tsx
+++ src/pages/Pos/DepositDevices.tsx
@@ -38,36 +38,15 @@ interface PosMonitoring {
     id: number;
     name: string;
     slug: string;
-    monthlyPlan: number;
-    timeWork: string;
+    address: string;
     organizationId: number;
-    posMetaData: string;
+    placementId: number;
     timezone: number;
-    image: string;
-    rating: number;
-    status: string;
+    posStatus: string;
     createdAt: Date;
     updatedAt: Date;
     createdById: number;
     updatedById: number;
-    address:
-    {
-        id: number;
-        city: string;
-        location: string;
-        lat: number;
-        lon: number;
-    };
-    posType:
-    {
-        id: number;
-        name: string;
-        slug: string;
-        carWashPosType: string;
-        minSumOrder: number;
-        maxSumOrder: number;
-        stepSumOrder: number;
-    };
 }
 
 const DepositDevices: React.FC = () => {
