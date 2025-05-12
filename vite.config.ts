c86e4567f4a95702092324d032282387ae8df655
devang-garg <devang@moy-ka.com>
2025-05-12 20:15:06 +0530

    fixing nginx issue.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  build: {
+    outDir: 'dist', // Ensure this matches your deployment settings
+  },
   plugins: [react(), svgr()],
   resolve: {
     alias: {
