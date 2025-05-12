ff31983749e84115d2a9491c78c215bd00702c5e
devang-garg <devang@moy-ka.com>
2025-05-12 19:22:16 +0530

    fixed route issue.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/onvi-monitoring-frontend/',
   plugins: [react(), svgr()],
   resolve: {
     alias: {
