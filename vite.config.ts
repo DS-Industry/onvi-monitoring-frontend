2bb15e05cbe6b649eba0b4ee6e292e89cccab48f
devang-garg <devang@moy-ka.com>
2025-05-13 15:51:24 +0530

    optimised initial loading time after deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
 import path from 'path';
+import viteImagemin from 'vite-plugin-imagemin';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +11,39 @@ export default defineConfig({
   build: {
     outDir: 'dist', // Ensure this matches your deployment settings
   },
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 75,
+      },
+      pngquant: {
+        quality: [0.65, 0.8],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+            active: false,
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false,
+          },
+        ],
+      },
+    }),
+    viteCompression()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
