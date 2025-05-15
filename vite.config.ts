631844732b880cb285cce4d8a65999532e69b992
devang-garg <devang@moy-ka.com>
2025-05-15 15:29:31 +0530

    solving refresh issue.

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   build: {
     outDir: 'dist', // Ensure this matches your deployment settings
   },
