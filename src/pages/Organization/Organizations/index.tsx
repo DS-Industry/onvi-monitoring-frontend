b0a82c07674b4f6d68e261469200a3e77267e853
devang-garg <devang@moy-ka.com>
2025-08-08 12:50:29 +0530

    Added headers to warehouse and made pos and administration responsive.

--- src/pages/Organization/Organizations/index.tsx
+++ src/pages/Organization/Organizations/index.tsx
@@ -156,7 +156,7 @@ const Organization: React.FC = () => {
 
   return (
     <>
-      <div className="ml-12 md:ml-0 mb-5 xs:flex xs:items-center xs:justify-between">
+      <div className="ml-12 md:ml-0 mb-5 xs:flex xs:items-start xs:justify-between">
         <div className="flex items-center space-x-2">
           <span className="text-xl sm:text-3xl font-normal text-text01">
             {t('routes.legalEntities')}
