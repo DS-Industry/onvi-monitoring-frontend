9f5dbc729c75c3a5bbe1bd3264455e6bb6f88988
Nikita Savitskiy <s6475515243@gmail.com>
2025-08-12 11:40:46 +0400

    started refactoring useAuthStore

--- src/config/store/authSlice.ts
+++ src/config/store/authSlice.ts
@@ -8,67 +8,21 @@ type Permission = {
 interface AuthState {
   isAuthenticated: boolean;
   permissions: Permission[];
-  posType: number | string;
-  startDate: Date;
-  endDate: Date;
-  deviceId: number | undefined;
-  documentType: string;
-  wareHouseId: number | string;
-  pageNumber: number;
-  currentPage: number;
-  pageSize: number;
-  city: number | string;
   setAuthenticated: (isAuthenticated: boolean) => void;
   logout: () => void;
   setPermissions: (permissions: Permission[]) => void;
-  setPosType: (posType: number | string) => void;
-  setStartDate: (startDate: Date) => void;
-  setEndDate: (endDate: Date) => void;
-  setDeviceId: (deviceId: number | undefined) => void;
-  setDocumentType: (documentType: string) => void;
-  setWareHouseId: (wareHouseId: number | string) => void;
-  setPageNumber: (pageNumber: number) => void;
   clearPermissions: () => void;
-  setCurrentPage: (currentPage: number) => void;
-  setPageSize: (pageSize: number) => void;
-  setCity: (city: number | string) => void;
   reset: () => void;
 }
-
-const today = new Date();
-const formattedDate = today.toISOString().slice(0, 10);
-const startDate = new Date(`${formattedDate} 00:00`);
-const endDate = new Date(`${formattedDate} 23:59`);
 
 const createAuthStore: StateCreator<AuthState> = set => {
   const initialState: AuthState = {
     isAuthenticated: false,
     permissions: [],
-    posType: '*',
-    startDate: startDate,
-    endDate: endDate,
-    deviceId: undefined,
-    documentType: '',
-    wareHouseId: '*',
-    pageNumber: 15,
-    currentPage: 1,
-    pageSize: 10,
-    city: '*',
     setAuthenticated: isAuthenticated => set({ isAuthenticated }),
     logout: () => set({ isAuthenticated: false, permissions: [] }),
     setPermissions: permissions => set({ permissions }),
-    setPosType: posType => set({ posType }),
-    setStartDate: startDate => set({ startDate }),
-    setEndDate: endDate => set({ endDate }),
-    setDeviceId: deviceId => set({ deviceId }),
-    setDocumentType: documentType => set({ documentType }),
-    setWareHouseId: wareHouseId => set({ wareHouseId }),
-    setPageNumber: pageNumber => set({ pageNumber }),
     clearPermissions: () => set(() => ({ permissions: [] })),
-    setCurrentPage: currentPage => set({ currentPage }),
-    setPageSize: pageSize => set({ pageSize }),
-    setCity: city => set({ city }),
-
     // Reset function
     reset: () => set(initialState),
   };
--- src/hooks/useAuthStore.ts
+++ src/hooks/useAuthStore.ts
@@ -28,94 +28,14 @@ export const useCheckAuth = () => {
   };
 };
 
-export const usePosType = () => {
-  return useAuthStore(state => state.posType);
-};
-
-export const useStartDate = () => {
-  return useAuthStore(state => state.startDate);
-};
-
-export const useEndDate = () => {
-  return useAuthStore(state => state.endDate);
-};
-
-export const useDeviceId = () => {
-  return useAuthStore(state => state.deviceId);
-};
-
-export const useSetPosType = () => {
-  return useAuthStore(state => state.setPosType);
-};
-
-export const useSetStartDate = () => {
-  return useAuthStore(state => state.setStartDate);
-};
-
-export const useSetEndDate = () => {
-  return useAuthStore(state => state.setEndDate);
-};
-
-export const useSetDeviceId = () => {
-  return useAuthStore(state => state.setDeviceId);
-};
-
 export const usePermissions = () => {
   return useAuthStore(state => state.permissions);
-};
-
-export const useWareHouseId = () => {
-  return useAuthStore(state => state.wareHouseId);
-};
-
-export const usePageNumber = () => {
-  return useAuthStore(state => state.pageNumber);
-};
-
-export const useCurrentPage = () => {
-  return useAuthStore(state => state.currentPage);
-};
-
-export const usePageSize = () => {
-  return useAuthStore(state => state.pageSize);
-};
-
-export const useCity = () => {
-  return useAuthStore(state => state.city);
 };
 
 export const useSetPermissions = () => {
   return useAuthStore(state => state.setPermissions);
 };
 
-export const useDocumentType = () => {
-  return useAuthStore(state => state.documentType);
-};
-
-export const useSetDocumentType = () => {
-  return useAuthStore(state => state.setDocumentType);
-};
-
-export const useSetWareHouseId = () => {
-  return useAuthStore(state => state.setWareHouseId);
-};
-
-export const useSetPageNumber = () => {
-  return useAuthStore(state => state.setPageNumber);
-};
-
 export const useClearPermissions = () => {
   return useAuthStore(state => state.clearPermissions);
-};
-
-export const useSetCurrentPage = () => {
-  return useAuthStore(state => state.setCurrentPage);
-};
-
-export const useSetPageSize = () => {
-  return useAuthStore(state => state.setPageSize);
-};
-
-export const useSetCity = () => {
-  return useAuthStore(state => state.setCity);
 };
