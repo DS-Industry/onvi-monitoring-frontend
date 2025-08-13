e5ef576d8a3bcecde8c9f64c557ef00e737a612f
devang-garg <devang@moy-ka.com>
2025-08-13 17:34:03 +0530

    Removed the unnecessary context

--- src/components/context/useContext.tsx
+++ src/components/context/useContext.tsx
@@ -1,22 +1,7 @@
 import { useContext } from 'react';
 import {
-  ButtonCreateContext,
-  FilterContext,
-  FilterOpenContext,
-  ToastContext,
+  ToastContext
 } from './Context.tsx';
-
-export const useButtonCreate = () => {
-  return useContext(ButtonCreateContext);
-};
-
-export const useFilterOpen = () => {
-  return useContext(FilterContext);
-};
-
-export const useFilterOn = () => {
-  return useContext(FilterOpenContext);
-};
 
 export const useToast = () => {
   const context = useContext(ToastContext);
