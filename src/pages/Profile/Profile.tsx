d767e66965a330df6b680067972814825b0e57ad
devang-garg <devang@moy-ka.com>
2025-08-18 11:48:06 +0530

    Added header to profile page

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 const InfoTab = React.lazy(() => import('./InfoTab'));
 const ServicesTab = React.lazy(() => import('./ServicesTab'));
 import { useTranslation } from 'react-i18next';
+import QuestionMarkIcon from '@icons/qustion-mark.svg?react';
 
 const ProfileForm: React.FC = () => {
   const { t } = useTranslation();
@@ -13,20 +14,30 @@ const ProfileForm: React.FC = () => {
   ];
 
   return (
-    <div className="max-w-5xl sm:ml-20 bg-white">
-      <div className="flex space-x-4 border-b mb-6 w-fit">
-        {tabs.map(tab => (
-          <button
-            key={tab.id}
-            className={`pb-2 ${activeTab === tab.id ? 'text-text01 border-b-4 border-primary02' : 'text-text02'}`}
-            onClick={() => setActiveTab(tab.id)}
-          >
-            {tab.name}
-          </button>
-        ))}
+    <div>
+      <div className="ml-12 sm:ml-20 mb-5">
+        <div className="flex items-center space-x-2">
+          <span className="text-xl sm:text-3xl font-normal text-text01">
+            {t('routes.profile')}
+          </span>
+          <QuestionMarkIcon />
+        </div>
       </div>
-      <div className="flex space-x-10">
-        {tabs.find(tab => tab.id === activeTab)?.content}
+      <div className="max-w-5xl sm:ml-20 bg-white">
+        <div className="flex space-x-4 border-b mb-6 w-fit">
+          {tabs.map(tab => (
+            <button
+              key={tab.id}
+              className={`pb-2 ${activeTab === tab.id ? 'text-text01 border-b-4 border-primary02' : 'text-text02'}`}
+              onClick={() => setActiveTab(tab.id)}
+            >
+              {tab.name}
+            </button>
+          ))}
+        </div>
+        <div className="flex space-x-10">
+          {tabs.find(tab => tab.id === activeTab)?.content}
+        </div>
       </div>
     </div>
   );
