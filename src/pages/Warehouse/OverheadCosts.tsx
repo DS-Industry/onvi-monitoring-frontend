f436b05a190aa06f9085582ebfae7feab1d4a373
devang-garg <devang@moy-ka.com>
2025-08-18 13:53:01 +0530

    Made warehouse headers responsive

--- src/pages/Warehouse/OverheadCosts.tsx
+++ src/pages/Warehouse/OverheadCosts.tsx
@@ -14,7 +14,6 @@ import GeneralFilters from '@/components/ui/Filter/GeneralFilters';
 import { useColumnSelector } from '@/hooks/useTableColumnSelector';
 import ColumnSelector from '@/components/ui/Table/ColumnSelector';
 import { ColumnsType } from 'antd/es/table';
-import QuestionMarkIcon from '@icons/qustion-mark.svg?react';
 
 type StockLevel = {
   nomenclatureId: number;
@@ -207,7 +206,6 @@ const OverheadCosts: React.FC = () => {
           <span className="text-xl sm:text-3xl font-normal text-text01">
             {t('routes.left')}
           </span>
-          <QuestionMarkIcon />
         </div>
       </div>
       <GeneralFilters count={transformedData.length} display={['city']}>
