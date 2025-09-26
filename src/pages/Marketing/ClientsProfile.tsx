5bbf1f26b41fe47dc8e8c9703c5cae73b27b947d
Nikita Savitskiy <s6475515243@gmail.com>
2025-09-26 09:42:40 +0400

    added isValidating to client profile page

--- src/pages/Marketing/ClientsProfile.tsx
+++ src/pages/Marketing/ClientsProfile.tsx
@@ -30,7 +30,7 @@ const ClientsProfile: React.FC = () => {
     ? Number(searchParams.get('userId'))
     : undefined;
 
-  const { data: clientData } = useSWR(
+  const { data: clientData, isLoading, isValidating } = useSWR(
     userId ? [`get-client-by-id`] : null,
     () => getClientById(userId!),
     {
@@ -40,6 +40,9 @@ const ClientsProfile: React.FC = () => {
       shouldRetryOnError: false
     }
   );
+
+  const showLoading = isLoading || isValidating;
+
 
   const tabItems = [
     {
@@ -98,7 +101,7 @@ const ClientsProfile: React.FC = () => {
         <div className="ml-12 md:ml-0 mb-5">
           <div className="flex items-center space-x-2">
             <span className="text-xl sm:text-3xl font-normal text-text01">
-              {t('marketing.client')} {clientData?.name}
+              {t('marketing.client')} {showLoading ? <Skeleton.Input active size="small" style={{ width: 200, height: 28 }} /> : clientData?.name}
             </span>
           </div>
         </div>
