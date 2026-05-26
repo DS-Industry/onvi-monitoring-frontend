a85a88d4cf73e68d14db84cfd9ab690c188670a0
Nikita Savitskiy <s6475515243@gmail.com>
2026-05-26 15:55:29 +0400

    hot fix

--- src/pages/Finance/SaleDocument/SaleDocument.tsx
+++ src/pages/Finance/SaleDocument/SaleDocument.tsx
@@ -110,7 +110,7 @@ const SaleDocument: React.FC = () => {
       <div className="ml-12 md:ml-0 mb-5 flex items-start justify-between">
         <div className="flex items-center space-x-2">
           <span className="text-xl sm:text-3xl font-normal text-text01">
-            {t('routes.saleDocument')} asas
+            {t('routes.saleDocument')}
           </span>
         </div>
         {canCreate && (
